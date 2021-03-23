f3bf62ce6ff9d0c03e2f48618a6428d0461affcd
rouxcaesar <jrzerwick@gmail.com>
2021-03-22 23:13:46 -0400

    Fixed bug in Load case logic

--- systems/vm-exercise/main.go
+++ systems/vm-exercise/main.go
@@ -9,24 +9,31 @@ import (
 func main() {
 	memory := make([]byte, 256)
 	memory[1] = 0x01
+	memory[1] = 0x03
 	memory[8] = 0x01
 	memory[9] = 0x01
 	memory[10] = 0x01
-	memory[11] = 0xff
-	//	memory[12] = 0x02
-	//	memory[13] = 0x01
-	//	memory[14] = 0x00
-	//	memory[15] = 0xff
+	memory[11] = 0x01
+	memory[12] = 0x02
+	memory[13] = 0x02
+	memory[14] = 0x02
+	memory[15] = 0x01
+	memory[16] = 0x00
+	memory[17] = 0x03
+	memory[18] = 0x01
+	memory[19] = 0x02
+	memory[20] = 0x04
+	memory[21] = 0x01
+	memory[22] = 0x02
+	memory[23] = 0xff
 
-	for i := 0; i <= 11; i++ {
+	for i := 0; i <= 20; i++ {
 		fmt.Printf("memory[%d]: %x\n", i, memory[i])
 	}
 
-	fmt.Println("\n")
-
 	vm.Compute(memory)
 
-	for i := 0; i <= 11; i++ {
+	for i := 0; i <= 20; i++ {
 		fmt.Printf("memory[%d]: %x\n", i, memory[i])
 	}
 }
--- systems/vm-exercise/vm/vm.go
+++ systems/vm-exercise/vm/vm.go
@@ -1,6 +1,9 @@
 package vm
 
-import "fmt"
+import (
+	"fmt"
+	_ "time"
+)
 
 const (
 	Load  = 0x01
@@ -32,6 +35,10 @@ func compute(memory []byte) {
 	// PC register is set to value 8 so that it starts at
 	// the first byte representing the first instruction
 	// in the memory slice.
+	//	fmt.Println("Memory before:\n")
+	for i := 0; i < 3; i++ {
+		fmt.Printf("memory[%d]: %x\n", i, memory[i])
+	}
 	registers := [3]byte{8, 0, 0} // PC, R1 and R2
 	pc := &registers[0]
 	//r1 := &registers[1]
@@ -49,26 +56,27 @@ func compute(memory []byte) {
 		// The opcode represents the instruction to be executed.
 		op := memory[*pc]
 		fmt.Printf("op: %v\n\n", op)
-		//		fmt.Printf("Load: %v\n\n", Load)
+		//time.Sleep(3 * time.Second)
 
 		switch op {
 		case Load:
 			// Copy data value and store in register, overwriting previous register value.
-
 			// Move PC forward and grab register to store data in.
 			*pc += 1
+			fmt.Printf("*pc = %v\n", *pc)
 			reg := memory[*pc]
-
+			fmt.Printf("reg: %v\n", reg)
 			// Move PC forward and access data in memory location.
 			*pc += 1
-			data := memory[*pc]
-
+			fmt.Printf("*pc = %v\n", *pc)
+			dataLocation := memory[*pc]
+			data := memory[dataLocation]
+			fmt.Printf("data to be loaded: %v\n", data)
 			// Set the register value to data value.
 			registers[reg] = data
-
 			// Move PC forward to next instruction.
 			*pc += 1
-
+			fmt.Printf("*pc = %v\n", *pc)
 			fmt.Printf("registers after Load: %v\n\n", registers)
 		case Store:
 			// Copy data value from register and store in memory location, overwriting
@@ -76,17 +84,61 @@ func compute(memory []byte) {
 			//
 			// The memory location should always be 0x00.
 			// If it is not, then we should end execution, possibly return an error value.
+			*pc += 1
+			fmt.Printf("*pc = %v\n", *pc)
+			reg := memory[*pc]
+			fmt.Printf("reg: %v\n", reg)
+			// Move PC forward and store data from register in memory location.
+			*pc += 1
+			fmt.Printf("*pc = %v\n", *pc)
+
+			location := memory[*pc]
+			fmt.Printf("location: %v\n", location)
+			if location != 0x00 {
+				fmt.Printf("Location is not the first byte in memory\n\n")
+				return
+			}
+
+			memory[location] = registers[reg]
+			fmt.Printf("first byte: %v\n", memory[location])
+
+			*pc += 1
+			fmt.Printf("*pc = %v\n", *pc)
+			fmt.Printf("registers after Store: %v\n\n", registers)
 		case Add:
 			// Set r1 = r1 + r2
+			*pc += 1
+			a1 := memory[*pc]
+
+			*pc += 1
+			a2 := memory[*pc]
+
+			registers[a1] = registers[a1] + registers[a2]
+
+			*pc += 1
+			fmt.Printf("registers after Add: %v\n\n", registers)
 		case Sub:
 			// Set r1 = r1 - r2
+			*pc += 1
+			a1 := memory[*pc]
+
+			*pc += 1
+			a2 := memory[*pc]
+
+			registers[a1] = registers[a1] - registers[a2]
+
+			*pc += 1
+			fmt.Printf("registers after Sub: %v\n\n", registers)
 		case Halt:
 			// End execution.
+			//fmt.Println("Memory after:\n")
+			//for i := 0; i < 4; i++ {
+			//	fmt.Printf("memory[%d]: %x\n", i, memory[i])
+			//}
+			//fmt.Println("\n")
 			return
 		}
 	}
-
-	return
 }
 
 // Compute calls the private compute function.
