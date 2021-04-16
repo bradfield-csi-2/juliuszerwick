02281c570f028bbc991d6eb9da164b23a64a360a
rouxcaesar <jrzerwick@gmail.com>
2021-04-16 09:42:50 -0400

    Wrote comments for implementing multiple commands with &&, ||, and ; symbols

--- systems/control-flow/shell/main.go
+++ systems/control-flow/shell/main.go
@@ -42,6 +42,23 @@ func main() {
 	}
 }
 
+/* For part 5 - Multiple Commands:
+- extract cmd variable creation and operations into a separate function
+- have the processArgs function handle tokenizing the user input
+- after splitting input by strings, iterate through array of input
+		- set first element to the "cmd" -> Ex: echo, whoami, cat
+		- set following elements [1:] to args to "cmd"
+		- if the following characters are found, mark as starting point for new cmd:
+				- &&, ||, ;
+		- For &&, the following "cmd" + args will be started after the previous cmd
+			returns with a zero exit code.
+		- For ||, the following "cmd" + args will be started after the previous cmd,
+			but the previous cmd's stdout and stdin will need to be connected to the
+			following cmd's stdout and stdin. Each subsequent cmd will run after the
+			previous cmd returns a non-zero exit status.
+		- For ;, simply execute each process sequentially regardless of exit code
+			returned from each cmd.
+*/
 func processArgs(args []string) {
 	var cmd *exec.Cmd
 
