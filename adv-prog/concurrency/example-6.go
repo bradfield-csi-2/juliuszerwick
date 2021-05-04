e9b138866e2ac7f448c8a96ff976a4402269c396
rouxcaesar <jrzerwick@gmail.com>
2021-05-03 22:04:37 -0400

    Finished example-6.go of concurrency prework

--- adv-prog/concurrency/example-6.go
+++ adv-prog/concurrency/example-6.go
@@ -21,14 +21,23 @@ func (c *coordinator) logState() {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
 
+	// Prints a single quoted character.
 	fmt.Printf("leader = %q\n", c.leader)
 }
 
 func (c *coordinator) setLeader(leader string, shouldLog bool) {
+	// The bug is due to the deferred c.lock.Unlock() which will execute at the end
+	// of this function body.
+	// This leads to situations in which if shouldLog == true there will be a call
+	// to c.lock.RLock() in logState() without the necessary c.lock.Unlock() before
+	// it.
+	// The fix is to change the deferred c.lock.Unlock() into an explicit, non-deferred call
+	// directly after the assignment operation is complete.
 	c.lock.Lock()
-	defer c.lock.Unlock()
 
 	c.leader = leader
+
+	c.lock.Unlock()
 
 	if shouldLog {
 		c.logState()
