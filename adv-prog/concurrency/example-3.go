fbca3eb4e04ccb248c6f67595b2c68decfe4037b
rouxcaesar <jrzerwick@gmail.com>
2021-05-03 21:01:14 -0400

    Finished example-3.go of concurrency prework

--- adv-prog/concurrency/example-3.go
+++ adv-prog/concurrency/example-3.go
@@ -12,10 +12,23 @@ func main() {
 		"https://bradfieldcs.com/courses/networking/",
 		"https://bradfieldcs.com/courses/databases/",
 	}
+
+	/*
+		Same as with example-1, the loop iteration will complete before each of the
+		goroutines have a chance to increment the WaitGroup. And by doing so, the main
+		goroutine will reach wg.Wait() and observe that the counter is still zero.
+		This leads the main goroutine to continue to the final fmt.Println() call
+		without any of the url strings being printed.
+
+		To fix this bug, we need to move the wg.Add(1) call out of the goroutine body
+		and above the goroutine call within the loop. This will ensure that the loop
+		increments the counter and have wg.Wait() actually wait until each goroutine finishes
+		and decrements the counter.
+	*/
 	var wg sync.WaitGroup
 	for i := range urls {
+		wg.Add(1)
 		go func(i int) {
-			wg.Add(1)
 			// Decrement the counter when the goroutine completes.
 			defer wg.Done()
 
