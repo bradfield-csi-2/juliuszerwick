d01b7511ffa49058c98a284f9ea8623d295244c8
rouxcaesar <jrzerwick@gmail.com>
2021-05-03 20:46:04 -0400

    Finished example-2.go of concurrency prework

--- adv-prog/concurrency/example-2.go
+++ adv-prog/concurrency/example-2.go
@@ -7,7 +7,18 @@ import (
 const numTasks = 3
 
 func main() {
-	var done chan struct{}
+	/*
+		The channel below is a nil channel.
+		All send and receive operations on a nil channel will block,
+		thus the code cannot continue past the send operation in
+		each goroutine.
+
+		To fix this, we need to use the make() built-in to create
+		an unbuffered channel that is ready to use.
+	*/
+
+	//var done chan struct{}
+	done := make(chan struct{})
 	for i := 0; i < numTasks; i++ {
 		go func() {
 			fmt.Println("running task...")
