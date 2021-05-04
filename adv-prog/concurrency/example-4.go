c96a65eb30f05309ec2eba01e98fb3dee1e15967
rouxcaesar <jrzerwick@gmail.com>
2021-05-03 21:16:24 -0400

    Finished example-4.go of concurrency prework

--- adv-prog/concurrency/example-4.go
+++ adv-prog/concurrency/example-4.go
@@ -4,13 +4,24 @@ import (
 	"fmt"
 )
 
+/*
+	The bug here is that the expected order of operations is not guaranteed.
+	With unbuffered channels, a send operation will always occur before a
+	receive operation on the same channel.
+
+	To fix this bug, we need to swap the locations of the send and receive
+	operations. The goroutine should send the token (struct{}{}) over the channel
+	and the main goroutine should receive the token. This ensure that the main
+	goroutine is blocked until the send operations is complete.
+*/
+
 func main() {
 	done := make(chan struct{}, 1)
 	go func() {
 		fmt.Println("performing initialization...")
-		<-done
+		done <- struct{}{}
 	}()
 
-	done <- struct{}{}
+	<-done
 	fmt.Println("initialization done, continuing with rest of program")
 }
