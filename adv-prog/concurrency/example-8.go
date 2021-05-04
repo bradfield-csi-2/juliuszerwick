519e277b106751bb1c817d7533abfd32ab837f9f
rouxcaesar <jrzerwick@gmail.com>
2021-05-03 22:50:59 -0400

    Finished example-8.go of concurrency prework

--- adv-prog/concurrency/example-8.go
+++ adv-prog/concurrency/example-8.go
@@ -25,11 +25,17 @@ func (d *dbService) logState() {
 	fmt.Printf("connection %q is healthy\n", d.connection)
 }
 
+// The bug here is nested locks (reentrant locks?) which
+// lead to a deadlock because the lock is never released.
+// To fix this bug, we need to explicitly call d.lock.RUnlock()
+// after the read is complete and before the call to d.logState()
 func (d *dbService) takeSnapshot() {
 	d.lock.RLock()
-	defer d.lock.RUnlock()
+	//defer d.lock.RUnlock()
 
 	fmt.Printf("Taking snapshot over connection %q\n", d.connection)
+
+	d.lock.RUnlock()
 
 	// Simulate slow operation
 	time.Sleep(time.Second)
