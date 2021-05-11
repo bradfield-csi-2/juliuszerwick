a689e85b09d8ee0491aae6b677b7544ec704ce46
rouxcaesar <jrzerwick@gmail.com>
2021-05-11 17:20:31 -0500

    Deleted function and struct bodies to write new implementation

--- adv-prog/mutex/mutex-impl.go
+++ adv-prog/mutex/mutex-impl.go
@@ -10,43 +10,18 @@ Test your implementation by using it to guard concurrent access to a counter var
 */
 
 type mutex struct {
-	locked bool
-	value  uint64
 }
 
 func (m *mutex) Lock() {
-	// Check if mutex is locked.
-	for {
-		if !m.locked {
-			m.locked = true
-			return
-		}
-		fmt.Println("mutex is already locked!")
-	}
-	// If false, update mutex state to locked.
-	// Else, block.
 }
 
 func (m *mutex) Unlock() {
-	// Check if mutex is locked.
-	for {
-		if m.locked {
-			m.locked = false
-			return
-		}
-		fmt.Println("mutex is not locked!")
-	}
-	// If true, update mutex state to unlocked.
-	// Else, block.
 }
 
 // Question: How do we make the Lock() and Unlock()
 //					 operations prevent access to the value
 //           by other goroutines/function calls?
 func increment(m *mutex) {
-	m.Lock()
-	m.value += 1
-	m.Unlock()
 }
 
 func main() {
