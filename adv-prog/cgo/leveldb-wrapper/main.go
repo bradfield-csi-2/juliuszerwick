3c691768e1a6d383e1fc0c616d19913765562df3
rouxcaesar <jrzerwick@gmail.com>
2021-05-24 22:16:18 -0400

    Succeeded in getting go vet to work after reinstalling leveldb with Homebrew and adding the correct CFLAGS and LDFLAGS directives

--- adv-prog/cgo/leveldb-wrapper/main.go
+++ adv-prog/cgo/leveldb-wrapper/main.go
@@ -1,47 +1,11 @@
 package main
 
 /*
-#cgo CFLAGS: -I/usr/include
+#cgo CFLAGS: -I/usr/local/Cellar/leveldb/1.22/include
+#cgo LDFLAGS: -L/usr/local/Cellar/leveldb/1.22/lib -lleveldb
 
 #include <stdlib.h>
-#include "leveldb/db.h"
-
-leveldb::DB* db;
-leveldb::Options options;
-options.create_if_missing = true;
-options.error_if_exists = true;
-
-int OpenDB(leveldb::DB* db) {
-	leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
-
-	if (!status.ok()) {
-		return 0;
-	}
-
-	return 1;
-}
-
-int CloseDB(leveldb::DB* db) {
-	delete db;
-	return 1;
-}
-
-int Put(leveldb::DB* db, std::string key, std::string value) {
-	leveldb::Status s = db->Put(leveldb::WriteOptions(), key, value);
-
-	if (!s.ok()) {
-		return 0;
-	}
-
-	return 1;
-}
-
-std::string Get(leveldb::DB* db, std::string key) {
-	std::string value;
-	leveldb::Status s = db->Get(leveldb::WriteOptions(), key, &value);
-
-	return value;
-}
+#include "leveldb/c.h"
 */
 import "C"
 
@@ -65,13 +29,14 @@ type Database struct {
 
 func main() {
 	// Open a db connection.
-	db := C.leveldb_t
-	dbName := "cgo-testdb"
-	i := Open(dbName)
+	fmt.Println("Opening a db connection")
+	//db := C.leveldb_t
+	//dbName := "cgo-testdb"
+	//i := Open(dbName)
 
-	if i == 0 {
-		log.Fatalln("failed to open db connection")
-	}
+	//if i == 0 {
+	//	log.Fatalln("failed to open db connection")
+	//}
 
 	// Make a Put into the db.
 
@@ -81,21 +46,21 @@ func main() {
 }
 
 // Open() opens a connection to the database.
-func Open(name string) (*Database, error) {
-	// Pass these options into Open() instead.
-	opts := C.leveldb_options_t
-	dbName := C.CString(name)
-	defer C.free(unsafe.Pointer(dbName))
-
-	ldb := C.leveldb_open(opts, dbName)
-
-	return &Database{ldb}, nil
-}
+//func Open(name string) (*Database, error) {
+// Pass these options into Open() instead.
+//	opts := C.leveldb_options_t
+//	dbName := C.CString(name)
+//	defer C.free(unsafe.Pointer(dbName))
+//
+//	ldb := C.leveldb_open(opts, dbName)
+//
+//	return &Database{ldb}, nil
+//}
 
 // Close() closes a connection to the database.
-func Close(db *Database) error {
-	C.leveldb_close(db)
-}
+//func Close(db *Database) error {
+//	C.leveldb_close(db)
+//}
 
 // Put() inserts a value into the database.
 //func Put(db *Database, key string, value string) error {
