cc160e12cf3008ed8ab4cebfedabfd66af1b731b
rouxcaesar <jrzerwick@gmail.com>
2021-06-10 12:51:56 -0400

    Latest changes

--- networks/dns/dns.go
+++ networks/dns/dns.go
@@ -1,21 +1,10 @@
 package main
 
-/*
-	This is a DNS client that constructs a DNS request and transmits it through a socket
-	to ensure the proper encapsulation by the transport, network, and link layer protocols.
-	It will also parse the response back from the DNS server.
-
-	TODOS:
-		1) Write a struct to represent the DNS query.
-		2) Write a struct to represent the DNS response.
-		3) Add logic to construct the DNS query and print to verify.
-		4) Add logic to create a datagram socket and write the DNS query to.
-		5) Add logic to receive the response and parse it into the DNS response struct.
-		6) Print the DNS response to verify.
-		7) Refactor code to allow user to specify the domain name and query type (turn it into a CLI tool).
-*/
-
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 var (
 	domainName = "https://en.wikipedia.org/wiki/Main_Page"
@@ -64,9 +53,32 @@ type resource_record struct {
 func main() {
 	fmt.Printf("Welcome to our DNS client!\n\n")
 
+	dnsMsg := dns_message{
+		header: {
+			id:     uint16(1),
+			qr:     uint8(0),
+			opcode: 0,
+		},
+	}
+
 	// Creating a socket for UDP and IPv4.
-	c, err := Dial("udp4", domainName)
+	c, err := net.Dial("udp4", domainName)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer c.Close()
+
+	// Write DNS request to Conn instance c.
+	_, err := c.Write()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read DNS response from Conn instance c.
+	resp, err := ioutil.ReadAll(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(resp))
 }
