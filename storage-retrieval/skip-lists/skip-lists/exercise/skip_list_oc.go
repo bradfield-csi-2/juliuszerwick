a08b83a41eb0c3217b7f02d171dfa317643683b6
rouxcaesar <jrzerwick@gmail.com>
2021-07-11 21:12:44 -0400

    Started prework for skip list class: wrote newSkipListOC() logic and commented out RangeScan code in main.go

--- storage-retrieval/skip-lists/skip-lists/exercise/skip_list_oc.go
+++ storage-retrieval/skip-lists/skip-lists/exercise/skip_list_oc.go
@@ -1,13 +1,47 @@
 package main
 
+type linkedNode struct {
+	item Item
+	next *linkedNode
+	prev *linkedNode
+	up   *linkedNode
+	down *linkedNode
+}
+
+type list struct {
+	head *linkedNode
+	tail *linkedNode
+}
+
 type skipListOC struct {
+	lists []*list
 }
 
 func newSkipListOC() *skipListOC {
-	return &skipListOC{}
+	head := &linkedNode{}
+	tail := &linkedNode{}
+	head.next = tail
+	tail.prev = head
+	newList := list{
+		head: head,
+		tail: tail,
+	}
+
+	return &skipListOC{
+		lists: []list{newList},
+	}
 }
 
 func (o *skipListOC) Get(key string) (string, bool) {
+	// First, get the length of the skipListOC list.
+	// The length - 1 will be the max level to start at.
+	maxLevel := len(o) - 1
+
+	// Iterate through linked lists in skip list from highest level to lowest.
+	// If we finish searching the lowest linked list without success, return "", false
+	for i := maxLevel; i >= 0; i-- {
+		// Iterate through linked list to find
+	}
 	return "", false
 }
 
@@ -19,6 +53,7 @@ func (o *skipListOC) Delete(key string) bool {
 	return false
 }
 
+// Below this line is if you have time.
 func (o *skipListOC) RangeScan(startKey, endKey string) Iterator {
 	return &skipListOCIterator{}
 }
