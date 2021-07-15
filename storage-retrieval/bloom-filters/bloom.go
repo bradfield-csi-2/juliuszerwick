9871b22576102d91d3f29dfe1e4c3895ea09067f
rouxcaesar <jrzerwick@gmail.com>
2021-07-15 09:36:03 -0400

    Wrote first draft of Bloom Fileter methods using simple hash functions

--- storage-retrieval/bloom-filters/bloom.go
+++ storage-retrieval/bloom-filters/bloom.go
@@ -29,12 +29,74 @@ func newBloomFilter() *myBloomFilter {
 	}
 }
 
+// Add item to BF.
 func (b *myBloomFilter) add(item string) {
-	// Add item to BF.
+	// Take item argument and extract a number for hashing.
+	r := []rune(item)
+
+	sum := 0
+	for _, i := range r {
+		sum += int(i)
+	}
+
+	l := len(b.data)
+
+	// Hash it once.
+	h1 := hashOne(sum, l)
+
+	// Hash it twice.
+	h2 := hashTwo(sum, l)
+
+	// Hash it thrice.
+	h3 := hashThree(h1, h2, l)
+
+	// Take each resultant value from hashing and set that index
+	// in Bloom Filter array to 1.
+	b.data[h1] = 1
+	b.data[h2] = 1
+	b.data[h3] = 1
+}
+
+func hashOne(num, m int) int {
+	return num % m
+}
+
+func hashTwo(num, m int) int {
+	return (num * 17) % m
+}
+
+func hashThree(a, b, m int) int {
+	return (a + (b * 71)) % m
 }
 
 func (b *myBloomFilter) maybeContains(item string) bool {
+	// Take item argument and extract a number for hashing.
+	r := []rune(item)
+
+	sum := 0
+	for _, i := range r {
+		sum += int(i)
+	}
+
+	l := len(b.data)
+
+	// Hash it once.
+	h1 := hashOne(sum, l)
+
+	// Hash it twice.
+	h2 := hashTwo(sum, l)
+
+	// Hash it thrice.
+	h3 := hashThree(h1, h2, l)
+
 	// If item's hashed positions are 1, might exist and return true.
+	bit1 := b.data[h1]
+	bit2 := b.data[h2]
+	bit3 := b.data[h3]
+
+	if bit1 == 1 && bit2 == 1 && bit3 == 1 {
+		return true
+	}
 
 	// Else, if even one of the hased positions is 0, does not exist and return false.
 	return false
