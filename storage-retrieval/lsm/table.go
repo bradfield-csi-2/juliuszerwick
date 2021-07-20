94537a40ede1e9c864bab8a75ac8a309b1d6c1b6
rouxcaesar <jrzerwick@gmail.com>
2021-07-20 09:06:53 -0400

    Wrote Build function

--- storage-retrieval/lsm/table.go
+++ storage-retrieval/lsm/table.go
@@ -1,11 +1,40 @@
 package table
 
+import (
+	"encoding/json"
+	"log"
+)
+
 type Item struct {
 	Key, Value string
 }
 
 // Given a sorted list of key/value pairs, write them out according to the format you designed.
 func Build(path string, sortedItems []Item) error {
+	t := &Table{
+		Data: sortedItems,
+	}
+
+	// Build Table Index by setting map keys to Item.Key and map values (offsets) to Item's position in slice.
+	for i, v := range sortedItems {
+		t.Index[v.Key] = i
+	}
+
+	// Encode values into buffer
+	// We use JSON just to create an MVP and can use the encoding/gob package to encode/decode binary data later.
+	jsonData, err := json.Marshal(t)
+	if err != nil {
+		log.Printf("JSON Marshal error: %v\n", err)
+		return err
+	}
+
+	// Write to file
+	err = ioutil.WriteWrite(path, jsonData, 0644)
+	if err != nil {
+		log.Printf("JSON Marshal error: %v\n", err)
+		return err
+	}
+
 	return nil
 }
 
@@ -16,7 +45,15 @@ func Build(path string, sortedItems []Item) error {
 // some metadata to help with efficient access (e.g. size, index, optional Bloom filter).
 type Table struct {
 	// TODO
+	//idx  *SSIndex
+	Index map[string]int
+	Data  []Item
 }
+
+//type SSIndex struct {
+//	key    string
+//	offset int
+//}
 
 // Prepares a Table for efficient access. This will likely involve reading some metadata
 // in order to populate the fields of the Table struct.
